29ef05da1228758b75d492aa14ebfd17f9db6b13
Allan Prado <allap.vba@gmail.com>
2024-11-24 23:58:31 -0300

    refactor(combobox-format): change combobox to unique react file and fix theme

--- src/components/combobox-format.tsx
+++ src/components/combobox-format.tsx
@@ -2,71 +2,114 @@
 
 import { cn } from "@/utils/tailwind-merge";
 import * as React from "react";
-import { PiCaretUpDownBold, PiCheckBold } from "react-icons/pi";
+import { PiCaretCircleRight, PiCaretUpDownBold, PiCheckBold } from "react-icons/pi";
 import { Button } from "./button";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "./command";
 import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js"
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit"
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js"
-  },
-  {
-    value: "remix",
-    label: "Remix"
-  },
-  {
-    value: "astro",
-    label: "Astro"
-  }
+const inputExtension = [
+  { value: "jpg", label: "JPG" },
+  { value: "JPEG", label: "JPEG" },
+  { value: "png", label: "PNG" }
 ];
 
+type ComboboxChildrenProps = {
+  state: string;
+  setState: React.Dispatch<React.SetStateAction<string>>;
+  setComboboxState: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
 export function ComboboxFormat() {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  const [inputOpen, setInputOpen] = React.useState(false);
+  const [outputOpen, setOutputOpen] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState("");
+  const [outputValue, setOutputValue] = React.useState("");
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between">
-          {value ? frameworks.find(framework => framework.value === value)?.label : "Select framework..."}
-          <PiCaretUpDownBold className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput placeholder="Search framework..." />
-          <CommandList>
-            <CommandEmpty>No framework found.</CommandEmpty>
-            <CommandGroup>
-              {frameworks.map(framework => (
-                <CommandItem
-                  key={framework.value}
-                  value={framework.value}
-                  onSelect={currentValue => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                  }}
-                >
-                  <PiCheckBold
-                    className={cn("mr-2 h-4 w-4", value === framework.value ? "opacity-100" : "opacity-0")}
-                  />
-                  {framework.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <div className="mb-8 flex gap-2 items-center justify-center">
+      <Popover open={inputOpen} onOpenChange={setInputOpen}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" role="combobox" aria-expanded={inputOpen} className="w-[200px] justify-between">
+            {inputValue
+              ? inputExtension.find(framework => framework.value === inputValue)?.label
+              : "Select extension..."}
+            <PiCaretUpDownBold className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+
+        <InputExtension setComboboxState={setInputOpen} setState={setInputValue} state={inputValue} />
+      </Popover>
+
+      <PiCaretCircleRight className="text-accent-9" />
+
+      <Popover open={outputOpen} onOpenChange={setOutputOpen}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" role="combobox" aria-expanded={outputOpen} className="w-[200px] justify-between">
+            {outputValue
+              ? inputExtension.find(framework => framework.value === outputValue)?.label
+              : "Select extension..."}
+            <PiCaretUpDownBold className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+
+        <OutputExtension setComboboxState={setOutputOpen} setState={setOutputValue} state={outputValue} />
+      </Popover>
+    </div>
+  );
+}
+
+function InputExtension({ setState, state, setComboboxState }: ComboboxChildrenProps) {
+  return (
+    <PopoverContent className="w-[200px] p-0">
+      <Command>
+        <CommandInput placeholder="Search extension..." />
+        <CommandList>
+          <CommandEmpty>No extension found.</CommandEmpty>
+          <CommandGroup>
+            {inputExtension.map(framework => (
+              <CommandItem
+                key={framework.value}
+                value={framework.value}
+                onSelect={currentValue => {
+                  setState(currentValue === state ? "" : currentValue);
+                  setComboboxState(false);
+                }}
+              >
+                <PiCheckBold className={cn("mr-2 h-4 w-4", state === framework.value ? "opacity-100" : "opacity-0")} />
+                {framework.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </PopoverContent>
+  );
+}
+
+function OutputExtension({ setState, state, setComboboxState }: ComboboxChildrenProps) {
+  return (
+    <PopoverContent className="w-[200px] p-0">
+      <Command>
+        <CommandInput placeholder="Search extension..." />
+        <CommandList>
+          <CommandEmpty>No extension found.</CommandEmpty>
+          <CommandGroup>
+            {inputExtension.map(framework => (
+              <CommandItem
+                key={framework.value}
+                value={framework.value}
+                onSelect={currentValue => {
+                  setState(currentValue === state ? "" : currentValue);
+                  setComboboxState(false);
+                }}
+              >
+                <PiCheckBold className={cn("mr-2 h-4 w-4", state === framework.value ? "opacity-100" : "opacity-0")} />
+                {framework.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </PopoverContent>
   );
 }
--- src/components/command.tsx
+++ src/components/command.tsx
@@ -12,10 +12,7 @@ const Command = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <CommandPrimitive
     ref={ref}
-    className={cn(
-      "flex h-full w-full flex-col overflow-hidden rounded-md bg-white text-neutral-950 dark:bg-neutral-950 dark:text-neutral-50",
-      className
-    )}
+    className={cn("flex h-full w-full flex-col overflow-hidden rounded-md bg-mauve-3 text-mauve-12", className)}
     {...props}
   />
 ));
@@ -27,7 +24,7 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-neutral-500 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5 dark:[&_[cmdk-group-heading]]:text-neutral-400">
+        <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-neutral-500 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
       </DialogContent>
@@ -44,7 +41,7 @@ const CommandInput = React.forwardRef<
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
-        "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:placeholder:text-neutral-400",
+        "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
@@ -81,7 +78,7 @@ const CommandGroup = React.forwardRef<
   <CommandPrimitive.Group
     ref={ref}
     className={cn(
-      "overflow-hidden p-1 text-neutral-950 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-neutral-500",
+      "overflow-hidden p-1 text-mauve-12 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-neutral-500",
       className
     )}
     {...props}
@@ -94,7 +91,7 @@ const CommandSeparator = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Separator>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Separator>
 >(({ className, ...props }, ref) => (
-  <CommandPrimitive.Separator ref={ref} className={cn("-mx-1 h-px bg-neutral-200", className)} {...props} />
+  <CommandPrimitive.Separator ref={ref} className={cn("-mx-1 h-px bg-mauve-6", className)} {...props} />
 ));
 CommandSeparator.displayName = CommandPrimitive.Separator.displayName;
 
@@ -105,7 +102,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-neutral-100 data-[selected=true]:text-neutral-900 data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+      "relative flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-mauve-12 data-[selected=true]:text-mauve-1 data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
       className
     )}
     {...props}
